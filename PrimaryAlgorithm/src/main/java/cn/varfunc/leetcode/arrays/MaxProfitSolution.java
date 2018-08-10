6787344da42628136e664df1391754d0075480f0
scapegoat <zfz2020@foxmail.com>
2018-08-10 08:43:22 +0800

    添加注释

--- PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/arrays/MaxProfitSolution.java
+++ PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/arrays/MaxProfitSolution.java
@@ -1,6 +1,15 @@
 package cn.varfunc.leetcode.arrays;
 
+/**
+ * 股票购买的最佳时机
+ * <p>
+ * 给定一个数组代表每一天的股价，每次只能进行一次交易，在一次交易完成之前不能进行下一次交易，求获得的最大利润
+ */
 public class MaxProfitSolution {
+    /**
+     * 一开始想的太复杂了，解法很简单
+     * 如果后一天的股价比前一天的高，就代表是有利可图的，把所有后一天股价比前一天的高的部分的差价求和就好了
+     */
     public int maxProfit(int[] prices) {
         int profit = 0;
         for (int i = 1; i < prices.length; i++) {
