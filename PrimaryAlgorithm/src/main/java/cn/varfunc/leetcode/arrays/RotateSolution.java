33f2dc86f745ef795b29bd280d0ee6ad4ba6b07c
scapegoat <zfz2020@foxmail.com>
2018-08-10 08:42:08 +0800

    重写解法并添加注释

--- PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/arrays/RotateSolution.java
+++ PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/arrays/RotateSolution.java
@@ -1,10 +1,13 @@
 package cn.varfunc.leetcode.arrays;
 
-import java.util.Arrays;
-import java.util.function.Consumer;
-
+/**
+ * 旋转数组
+ * <p>
+ * 给定一个数组和整数k，k代表把数组中每个元素向右移动的次数，求移动过后的数组
+ */
 public class RotateSolution {
     public void rotate(int[] nums, int k) {
+//        一开是自己想到的解法，没能通过
 //        if (nums.length == 2) {
 //            if (k % 2 != 0) {
 //                int tmp = nums[0];
@@ -21,8 +24,7 @@ public class RotateSolution {
 //            }
 //        }
 
-
-//        // 旋转K次，每次旋转1个单位，只可惜是见复杂度是n^2
+//        想到的另一种解法依旧没能通过
 //        for (int i = 0; i < k; i++) {
 //            Queue<Integer> queue = new LinkedList<>();
 //            int index = 0;
@@ -37,19 +39,29 @@ public class RotateSolution {
 //        }
 
 
-        // 翻转字符的方法，先把前n-k个字符翻转，再把后面k个字符翻转，最后再把整个字符翻转
-        // 定义一个翻转函数，以便重复使用
-        Consumer<int[]> reverse = it -> {
-            int n = it.length % 2;
-            for (int i = 0; i < n; i++) {
-                int tmp = it[i];
-                it[i] = it[it.length - i];
-                it[it.length - i] = tmp;
-            }
-        };
+        // 对k进行处理，把k映射到[0, 6]的区间上
+        int n = k % nums.length;
+        if (n < 0) {
+            n += nums.length;
+        }
 
-        reverse.accept(Arrays.copyOfRange(nums, 0, nums.length - k));
-        reverse.accept(Arrays.copyOfRange(nums, nums.length - k, nums.length));
-        reverse.accept(nums);
+        // 翻转字符的方法，先把前n-k个字符翻转，再把后面k个字符翻转，最后再把整个字符翻转
+        // 当n为零的时候不需要翻转，这里假定n位于[0, 6]的区间内
+        if (n != 0) {
+            reverse(nums, 0, nums.length - n);
+            reverse(nums, nums.length - n, nums.length);
+            reverse(nums, 0, nums.length);
+        }
+    }
+
+    /**
+     * 定义一个翻转函数，以便重复使用
+     */
+    private void reverse(int[] numbers, int start, int end) {
+        for (int i = 0, j = start, k = end - 1; i < (end - start) / 2; i++, j++, k--) {
+            int t = numbers[j];
+            numbers[j] = numbers[k];
+            numbers[k] = t;
+        }
     }
 }
