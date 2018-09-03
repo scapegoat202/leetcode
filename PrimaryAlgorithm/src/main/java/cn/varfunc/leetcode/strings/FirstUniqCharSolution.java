30ee02c506812985b095b0e86d7bbd6940773000
scapegoat <zfz2020@foxmail.com>
2018-09-03 22:23:28 +0800

    新增：“字符串里第一个唯一字符”的解法

---
+++ PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/strings/FirstUniqCharSolution.java
@@ -0,0 +1,40 @@
+package cn.varfunc.leetcode.strings;
+
+/**
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1
+ * <p>
+ * 可以假定该字符串只包含小写字母
+ */
+public class FirstUniqCharSolution {
+    public int firstUniqChar(String s) {
+        /* 使用Map的解法，在Map里key为s中存在的字符，value为其出现的次数 */
+//        Map<Character, Integer> map = new HashMap<>();
+//        for (int i = 0; i < s.length(); i++) {
+//            if (map.containsKey(s.charAt(i))) {
+//                map.put(s.charAt(i), map.get(s.charAt(i)) + 1);
+//            } else {
+//                map.put(s.charAt(i), 1);
+//            }
+//        }
+//
+//        int index = -1;
+//
+//        for (int i = 0; i < s.length(); i++) {
+//            if (map.get(s.charAt(i)) == 1) {
+//                index = i;
+//                break;
+//            }
+//        }
+//
+//        return index;
+
+        int result = -1;
+        for (char c = 'a'; c <= 'z'; c++) {
+            int index = s.indexOf(c);
+            if (index != -1 && index == s.lastIndexOf(c)) {
+                result = (result != -1) ? Math.min(result, index) : index;
+            }
+        }
+        return result;
+    }
+}
