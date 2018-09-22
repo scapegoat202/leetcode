dd79d012599227f0c989aed7d671b22cb0d27678
scapegoat <zfz2020@foxmail.com>
2018-09-22 15:01:53 +0800

    feat(PrimaryAlgorithm/DynamicPlanning): add code for Max Profit

---
+++ PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/dynamicplanning/MaxProfitSolution.java
@@ -0,0 +1,22 @@
+package cn.varfunc.leetcode.dynamicplanning;
+
+/**
+ * 买股票的最佳时机
+ * <p>
+ * 给定一个数组，它的第 i 个元素是一支给定股票第 i 天的价格。
+ * <p>
+ * 如果你最多只允许完成一笔交易（即买入和卖出一支股票），设计一个算法来计算你所能获取的最大利润。
+ * <p>
+ * 注意你不能在买入股票前卖出股票。
+ */
+public class MaxProfitSolution {
+    public int maxProfit(int[] prices) {
+        int profit = 0;
+        int min = Integer.MAX_VALUE;
+        for (int price : prices) {
+            min = Math.min(min, price);
+            profit = Math.max(profit, price - min);
+        }
+        return profit;
+    }
+}
