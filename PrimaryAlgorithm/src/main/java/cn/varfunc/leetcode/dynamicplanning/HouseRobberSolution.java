4576f568e020dc1af795699bb835584e9340c1c8
Edgar Green <zfz2020@foxmail.com>
2018-12-14 14:06:46 +0800

    feat(PrimaryAlgorithm/DynamicPlanning): add solution for house robber

---
+++ PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/dynamicplanning/HouseRobberSolution.java
@@ -0,0 +1,24 @@
+package cn.varfunc.leetcode.dynamicplanning;
+
+/**
+ * 打家劫舍
+ * <p>
+ * 你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，
+ * 如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+ * <p>
+ * 给定一个代表每个房屋存放金额的非负整数数组，计算你在不触动警报装置的情况下，能够偷窃到的最高金额。
+ */
+public class HouseRobberSolution {
+    public int rob(int[] nums) {
+        int a = nums[0];
+        int b = nums[1];
+        for (int i = 2; i < nums.length; i++) {
+            /* 如果选择了抢劫上一个屋子，那么就不能抢劫当前的屋子，所以最大收益就是抢劫上一个屋子的收益 */
+            /* 如果选择抢劫当前屋子，就不能抢劫上一个屋子，所以最大收益是到上一个屋子的上一个屋子为止的最大收益，加上当前屋子里有的钱 */
+            int temp = b;
+            b = Math.max(b, a + nums[i]);
+            a = temp;
+        }
+        return b;
+    }
+}
