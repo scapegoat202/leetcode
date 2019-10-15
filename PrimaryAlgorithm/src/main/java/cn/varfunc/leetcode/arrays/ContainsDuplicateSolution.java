f8b58eff136d774e427afb771e76185c2d525569
Edgar Green <zfz2020@foxmail.com>
2019-10-15 08:26:53 +0800

    feat(PrimaryAlgorithm/Arrays): Improve solution for Contains Duplicate

--- PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/arrays/ContainsDuplicateSolution.java
+++ PrimaryAlgorithm/src/main/java/cn/varfunc/leetcode/arrays/ContainsDuplicateSolution.java
@@ -1,17 +1,19 @@
 package cn.varfunc.leetcode.arrays;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 /**
  * 检查在一个数组中是否存在重复的元素
  */
 public class ContainsDuplicateSolution {
     public boolean containsDuplicate(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        for (int num : nums) {
-            if (!set.add(num)) {
-                return true;
+        final int length = nums.length;
+        if (length > 1) {
+            Arrays.sort(nums);
+            for (int i = 1; i < length; i++) {
+                if (nums[i] == nums[i - 1]) {
+                    return true;
+                }
             }
         }
         return false;
